d85ddda77d743f6d26b8c8353ba038b9c0937810
eltoncampos1 <qazwsx272518>
2020-06-11 03:41:44 -0300

    Simples todo list

---
+++ todos.js
@@ -0,0 +1,62 @@
+//selecionar os elementos
+
+var listElement = document.querySelector('#app ul')
+var inputElement = document.querySelector('#app input')
+var buttonElement = document.querySelector('#app button')
+
+//referenciar todos
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
+
+function renderTodos(){
+    listElement.innerHTML = '';
+    for (todo of todos){
+       
+
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        var linkElement = document.createElement('a');
+
+        linkElement.setAttribute('href', '#');
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodos('+ pos +')');
+
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText)
+
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+
+        listElement.appendChild(todoElement);
+
+    }
+}
+renderTodos();
+
+//adicionar todos
+
+function addTodos(){  
+    var todoText = inputElement.value;
+
+    todos.push(todoText)
+    inputElement.value = "";
+    renderTodos();
+    saveToStorage();
+}
+
+buttonElement.onclick = addTodos;
+
+//deletar todos
+
+function deleteTodos(pos) {
+    todos.splice(pos, 1);
+    renderTodos();
+    saveToStorage();
+}
+
+//salvar no storage
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos));
+}
